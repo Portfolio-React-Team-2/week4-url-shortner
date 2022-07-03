abe0c25e786bd5a2a08cae0fce422ccb87bd774b
roji-tech <rojitech9@gmail.com>
2022-07-03 12:51:50 +0100

    added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./home.css";
 import pic1 from "../images/FETEAME2.jpeg";
 import pic2 from "../images/FETAEME21.jpeg";
@@ -11,24 +11,32 @@ const Home = () => {
           <img src={pic2} alt="" />
         </div>
         <form action="" method="get">
-          <label htmlFor="url" id='label'>Enter your URL here</label>
+          <label htmlFor="url" id="label">
+            Enter your URL here
+          </label>
           <input type="url" name="url" id="url" />
-          <label htmlFor="link" id='label'>Customize your link</label>
-          <input type="url" name="url" id="url" placeholder='fe-team2shorturl.me' />
+          <label htmlFor="link" id="label">
+            Customize your link
+          </label>
+          <input
+            type="url"
+            name="url"
+            id="url"
+            placeholder="fe-team2shorturl.me"
+          />
           <div className="btns">
-            <a href="/" id='myurl'>My URL</a>
-            <button className='fet2-it'>FET2 It</button>
+            <a href="/" id="myurl">
+              My URL
+            </a>
+            <button className="fet2-it">FET2 It</button>
           </div>
         </form>
         <div className="sideimg">
           <img src={pic1} alt="" />
         </div>
       </div>
-
-
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
